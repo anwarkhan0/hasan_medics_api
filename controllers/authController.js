defb0f3437a174f88e0f21e8b644e5d93cd22c8c
anwarkhan0 <anwarsyed01234@gmail.com>
2025-08-09 12:38:07 +0300

    fix: login issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,9 +22,7 @@ exports.register = async (req, res) => {
     const existing = await User.findOne({ email });
     if (existing) return res.status(400).json({ message: 'Email already registered' });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({ name, email, password: hashedPassword });
+    const user = await User.create({ name, email, password: password });
     // After user.save()
     await sendEmail(
       user.email,
